85fb4122259af8324283be8fbc611143760149ed
null <null>
2025-08-17 15:49:11 +0530

    components

---
+++ src/components/Bye.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Bye(){
+    return (
+        <div>Bye component</div>
+    )
+}
+
+export default Bye
---
+++ src/components/Hello.jsx
@@ -0,0 +1,7 @@
+function Hello(){
+    return (
+        <h2>Hello from hello component</h2>
+    )
+}
+
+export default Hello
